454daf9df73d4285fbf0770e1aa44fbbbcafd8c3
Hye-In Yoon <heain1001@naver.com>
2026-01-04 17:23:24 +0900

    Style : 관리자 - 사이드바 디자인 변경

--- @frontend/app/admin/page.tsx
+++ @frontend/app/admin/page.tsx
@@ -1,6 +1,11 @@
 
 export default function AdminPage() {
   return (
-    "관리자 메인 대시보드입니다."
+    <>
+      <div>
+        관리자 메인 대시보드입니다
+      </div>
+      <div>추후 통계 같은게 들어갈 예정</div>
+    </>
   )
 }
