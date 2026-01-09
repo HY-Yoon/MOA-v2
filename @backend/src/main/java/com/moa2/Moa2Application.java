28cfe756c3f7225c32d662827df42e7aed94a9fb
DAHEE33 <todaykim10@gmail.com>
2026-01-09 17:04:34 +0900

    [BE] feat: WIP 배포 설정 적용2 (테스트)

--- @backend/src/main/java/com/moa2/Moa2Application.java
+++ @backend/src/main/java/com/moa2/Moa2Application.java
@@ -2,8 +2,9 @@ package com.moa2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {RedisAutoConfiguration.class})
 public class Moa2Application {
 
     public static void main(String[] args) {
