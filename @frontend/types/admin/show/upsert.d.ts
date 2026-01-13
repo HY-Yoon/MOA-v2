47c54b86e71fc059012a4006819580f3773fa472
Seongmin <chr92@naver.com>
2026-01-13 21:10:21 +0900

    feat: 공연 상세 조회 api 및 타입 추가

--- @frontend/types/admin/show/upsert.d.ts
+++ @frontend/types/admin/show/upsert.d.ts
@@ -1,30 +1,26 @@
-import { Genre, Region } from '@shared/enums';
+namespace ShowUpsertType {
+  interface Location {
+    region: import('@shared/enums').Region;
+    venueName: string;
+    hallName: string;
+  }
 
-declare global {
-  namespace ShowUpsertType {
-    interface Location {
-      region: Region;
-      venueName: string;
-      hallName: string;
-    }
+  interface Schedule {
+    showDate: Date;
+    showTime: string;
+    ticketOpenTime: Date;
+  }
 
-    interface Schedule {
-      showDate: Date;
-      showTime: string;
-      ticketOpenTime: Date; // YYYY-MM-DD HH:mm (시간 포함)
-    }
-
-    interface ShowUpsertForm {
-      title: string;
-      genre: Genre;
-      location: Location;
-      runningTime: string;
-      cast: string;
-      salePeriod: {
-        startDate: Date;
-        endDate: Date;
-      };
-      schedules: Schedule[];
-    }
+  interface ShowUpsertForm {
+    title: string;
+    genre: import('@shared/enums').Genre;
+    location: Location;
+    runningTime: string;
+    cast: string;
+    salePeriod: {
+      startDate: Date;
+      endDate: Date;
+    };
+    schedules: Schedule[];
   }
 }
