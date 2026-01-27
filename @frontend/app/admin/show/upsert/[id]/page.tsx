ac2eebae087d5f1825cdbb945c0e94e4fbb1f025
Seongmin <chr92@naver.com>
2026-01-27 23:01:19 +0900

    [FE] fix: 공연 등록/수정 페이지명 변경

--- @frontend/app/admin/show/upsert/[id]/page.tsx
+++ @frontend/app/admin/show/upsert/[id]/page.tsx
@@ -4,7 +4,7 @@ interface Props {
   params: Promise<{ id: string }>;
 }
 
-export default async function ShowUpsert({ params }: Props) {
+export default async function ShowUpdatePage({ params }: Props) {
   const { id } = await params;
   return <ShowUpsertForm id={id} />;
 }
