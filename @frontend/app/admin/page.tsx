0a866704fb6d2eff388872dabf4442b20d9def2f
Seongmin <chr92@naver.com>
2026-01-30 22:02:23 +0900

    [FE] fix: 관리자 메인 공연 목록 리다이렉트 임시 적용

--- @frontend/app/admin/page.tsx
+++ @frontend/app/admin/page.tsx
@@ -1,11 +1,15 @@
+import { redirect } from 'next/navigation';
+import { ADMIN_ROUTES } from '@/constants/route/adminRoutes';
 
 export default function AdminPage() {
-  return (
-    <>
-      <div>
-        관리자 메인 대시보드입니다
-      </div>
-      <div>추후 통계 같은게 들어갈 예정</div>
-    </>
-  )
+  // return (
+  //   <>
+  //     <div>
+  //       관리자 메인 대시보드입니다
+  //     </div>
+  //     <div>추후 통계 같은게 들어갈 예정</div>
+  //   </>
+  // )
+  // TODO: 관리자 메인 대시보드 추가 전까지 공연 목록으로 리다이렉트
+  redirect(ADMIN_ROUTES.SHOW);
 }
