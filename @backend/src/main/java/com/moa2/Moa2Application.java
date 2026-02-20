14214cd1bf6f601f4aa9d647741cf871527c6c26
DAHEE33 <todaykim10@gmail.com>
2026-02-20 12:41:59 +0900

    [BE] 버전관리 추가2

--- @backend/src/main/java/com/moa2/Moa2Application.java
+++ @backend/src/main/java/com/moa2/Moa2Application.java
@@ -3,10 +3,16 @@ package com.moa2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.redisson.spring.starter.RedissonAutoConfiguration;
+import org.redisson.spring.starter.RedissonAutoConfigurationV2;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@SpringBootApplication(exclude = {RedisAutoConfiguration.class})
+@SpringBootApplication(exclude = {
+        RedisAutoConfiguration.class,
+        RedissonAutoConfiguration.class,
+        RedissonAutoConfigurationV2.class
+})
 public class Moa2Application {
 
     public static void main(String[] args) {
