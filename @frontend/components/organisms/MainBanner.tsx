2eb23ab26733b540e12927745761a2aee55484c2
Seongmin <chr92@naver.com>
2026-04-08 14:18:58 +0900

    [FE] fix: 메인 배너 사이즈 수정 및 백그라운드 컬러 설정

--- @frontend/components/organisms/MainBanner.tsx
+++ @frontend/components/organisms/MainBanner.tsx
@@ -7,19 +7,28 @@ import Image from 'next/image';
 const banners = [
   {
     src: 'https://ticketimage.interpark.com/TCMS3.0/NMain/BbannerPC/2603/260312093256_26003451.gif',
-    alt: '메인 1',
+    alt: 'verdy',
+    bgColor: '#E7E8E7',
   },
   {
     src: 'https://ticketimage.interpark.com/TCMS3.0/NMain/BbannerPC/2603/260303083142_26002980.gif',
-    alt: '메인 2',
+    alt: '마리 로랑생',
+    bgColor: '#98A0A2',
   },
   {
     src: 'https://ticketimage.interpark.com/TCMS3.0/NMain/BbannerPC/2601/260129081837_26001001.gif',
-    alt: '메인 3',
+    alt: '빌리 엘리어트',
+    bgColor: '#F7F7F7',
   },
   {
     src: 'https://ticketimage.interpark.com/TCMS3.0/NMain/BbannerPC/2604/260407082558_26002992.gif',
-    alt: '메인 4',
+    alt: '엔하이픈',
+    bgColor: '#214552',
+  },
+  {
+    src: 'https://ticketimage.interpark.com/TCMS3.0/NMain/BbannerPC/2604/260402012900_P0004610.gif',
+    alt: '디즈니',
+    bgColor: '#000000',
   },
 ];
 
@@ -30,13 +39,18 @@ export default function MainBanner() {
     <div className="overflow-hidden" ref={emblaRef}>
       <div className="flex">
         {banners.map((banner, index) => (
-          <div key={index} className="relative h-72 min-w-full sm:h-96 lg:h-[480px]">
+          <div
+            key={index}
+            className="flex h-120 min-w-full items-center justify-center overflow-hidden"
+            style={{ backgroundColor: banner.bgColor }}
+          >
             <Image
               src={banner.src}
               alt={banner.alt}
-              fill
+              width={2000}
+              height={560}
               priority={index === 0}
-              className="object-cover object-top"
+              className="h-120 w-auto max-w-none object-contain object-center"
             />
           </div>
         ))}
